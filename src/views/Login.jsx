8c81364ca8ba5cf060c43b1650ac44f52d636018
ThomasGiardina <thomasagustingiardina@gmail.com>
2024-10-05 12:12:07 -0300

    Login

---
+++ src/views/Login.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import ContainerLogin from '../components/Login/ContainerLogin'
+
+function App() {
+
+return (
+    <> 
+      <Storenavbar />
+      <Carrousel />
+      <div className="flex ml-28 space-x-10 items-start max-w-7xl">
+        <Gamefilter />
+        <GameCard game={gameData1} />
+        <GameCard game={gameData2} />
+        <GameCard game={gameData3} />
+        <GameCard game={gameData4} />
+      </div>   
+    </>
+)
+}
+
+export default App
