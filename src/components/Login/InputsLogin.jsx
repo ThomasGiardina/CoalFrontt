b3df3b30aa854f668f0b8d521e676ac1b41d975d
ThomasGiardina <thomasagustingiardina@gmail.com>
2024-10-08 08:59:02 -0300

    Contraseña

--- src/components/Login/InputsLogin.jsx
+++ src/components/Login/InputsLogin.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 const InputsLogin = () => {
 
+    const [password, setPassword] = useState("");
+
     return (
         <div className="flex flex-col items-start gap-4"> 
             <label className="input input-bordered flex items-center gap-2 w-[520px]">
@@ -24,11 +26,17 @@ const InputsLogin = () => {
                     fill="currentColor"
                     className="h-4 w-4 opacity-70">
                     <path
-                    fillRule="evenodd"
-                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                    clipRule="evenodd" />
+                        fillRule="evenodd"
+                        d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                        clipRule="evenodd" />
                 </svg>
-                <input type="password" className="grow"  value="Contraseña" onChange={(e) => setPassword(e.target.value)} />
+                <input 
+                    type="password" 
+                    className="grow" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    placeholder="Contraseña" 
+                />
             </label>
         </div>
     )
