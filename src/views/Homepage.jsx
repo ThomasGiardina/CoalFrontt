0244ece474c1755bbc594cb23df9002a2610f2c5
MarcoAmbrosini22 <mambrosini@uade.edu.ar>
2024-10-10 13:07:57 -0300

    Home (ahora si)

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -7,7 +7,7 @@ import WhyCoalSection from "../components/Homeimg/WhyCoalSection";
 
 function Homepage() {
     return (
-        <div className="relative min-h-screen bg-black flex flex-col">
+        <div className="relative min-h-screen flex flex-col" style={{ background: 'linear-gradient(to bottom, #000000, #0d0d0d, #1a1a1a, #262626, #333333)' }}>
             <div className="w-full flex flex-1">
                 <div className="w-1/2 flex items-center justify-center">
                     <Homehero />
