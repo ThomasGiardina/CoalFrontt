45a097106a8b115585bb2da49994a19fc430b4be
MarcoAmbrosini22 <mambrosini@uade.edu.ar>
2024-10-10 12:47:37 -0300

    Home mejorada

---
+++ src/components/Homeimg/ConsolesSection.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import xboxImage from '../../assets/xbox.jpg';
+import playstationImage from '../../assets/playstation.jpg';
+import nintendoImage from '../../assets/nintendo.jpg';
+import pcImage from '../../assets/pc.jpg';
+
+function ConsolesSection() {
+    return (
+        <div className="w-full flex flex-col items-center mt-20">
+            <h2 className="text-2xl font-bold text-white mb-10">Elegí tu aventura</h2>
+            <div className="w-full flex justify-around mb-20">
+                <Link to="/tienda?filtro=xbox" className="console-box w-40 h-40">
+                    <img src={xboxImage} alt="XBOX" className="w-full h-full object-cover rounded-lg transition-transform transform hover:scale-110" />
+                </Link>
+                <Link to="/tienda?filtro=playstation" className="console-box w-40 h-40">
+                    <img src={playstationImage} alt="PLAYSTATION" className="w-full h-full object-cover rounded-lg transition-transform transform hover:scale-110" />
+                </Link>
+                <Link to="/tienda?filtro=nintendo" className="console-box w-40 h-40">
+                    <img src={nintendoImage} alt="NINTENDO SWITCH" className="w-full h-full object-cover rounded-lg transition-transform transform hover:scale-110" />
+                </Link>
+                <Link to="/tienda?filtro=pc" className="console-box w-40 h-40">
+                    <img src={pcImage} alt="PC" className="w-full h-full object-cover rounded-lg transition-transform transform hover:scale-110" />
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default ConsolesSection;
--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -1,17 +1,22 @@
-import "../index.css";
+import React from 'react';
 import Homehero from "../components/Hero/HomeHero";
 import Homeimg from "../components/Homeimg/Homeimg";
-
+import ConsolesSection from "../components/Homeimg/ConsolesSection";
+import FindUsSection from "../components/Homeimg/FindUsSection";
 
 function Homepage() {
     return (
-        <div className="relative min-h-screen bg-black flex">
-            <div className="w-1/2 flex items-center justify-center">
-                <Homehero />
+        <div className="relative min-h-screen bg-black flex flex-col">
+            <div className="w-full flex flex-1">
+                <div className="w-1/2 flex items-center justify-center">
+                    <Homehero />
+                </div>
+                <div className="relative w-1/2 flex justify-center items-center mr-5">
+                    <Homeimg /> 
+                </div>
             </div>
-            <div className="relative w-1/2 flex justify-center items-center mr-5">
-                <Homeimg /> 
-            </div>
+            <ConsolesSection />
+            <FindUsSection />
         </div>
     );
 }
