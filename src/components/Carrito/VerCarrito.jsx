2ded9e941d82b9cfcf4fed210ce284e891fbf1f0
ThomasGiardina <thomasagustingiardina@gmail.com>
2024-10-18 02:15:11 -0300

    CantidadTotalCarrito

--- src/components/Carrito/VerCarrito.jsx
+++ src/components/Carrito/VerCarrito.jsx
@@ -4,6 +4,7 @@ import GamecardCart from "../Gamecard/GamecardCart";
 const VerCarrito = ({ onContinue }) => {
     const [cartItems, setCartItems] = useState([]);
     const [total, setTotal] = useState(0);
+    const [totalProductos, setTotalProductos] = useState(0); 
     const token = localStorage.getItem('token');
 
     useEffect(() => {
@@ -12,22 +13,24 @@ const VerCarrito = ({ onContinue }) => {
                 const response = await fetch("http://localhost:4002/carritos/usuarios/carrito", {
                     headers: { Authorization: `Bearer ${token}` },
                 });
-    
+
                 if (response.ok) {
                     const data = await response.json();
-                    // Solo imprime los datos si realmente lo necesitas para depuración
                     console.log("Datos completos del carrito:", data);
-    
+
                     if (Array.isArray(data) && data.length > 0) {
                         const itemsConCarritoId = data.map(item => ({
                             ...item,
                             carrito_id: item.carritoId
                         }));
-    
+
                         setCartItems(itemsConCarritoId);
-    
+
                         const calculatedTotal = itemsConCarritoId.reduce((acc, item) => acc + item.precio * item.cantidad, 0);
                         setTotal(calculatedTotal);
+
+                        const totalCantidad = itemsConCarritoId.reduce((acc, item) => acc + item.cantidad, 0);
+                        setTotalProductos(totalCantidad);
                     } else {
                         console.error("El carrito está vacío o no se encontraron items.");
                     }
@@ -40,21 +43,21 @@ const VerCarrito = ({ onContinue }) => {
         };
         fetchCart();
     }, [token]);
-    
-    
-    
 
     const handleUpdateQuantity = (itemId, nuevaCantidad) => {
         const carritoId = cartItems.length > 0 ? cartItems[0].carrito_id : null;
-    
+
         if (carritoId) {
             const updatedItems = cartItems.map(item =>
                 item.id === itemId ? { ...item, cantidad: nuevaCantidad } : item
             );
             setCartItems(updatedItems);
-    
+
             const newTotal = updatedItems.reduce((acc, item) => acc + item.precio * item.cantidad, 0);
             setTotal(newTotal);
+
+            const newTotalProductos = updatedItems.reduce((acc, item) => acc + item.cantidad, 0);
+            setTotalProductos(newTotalProductos);
         } else {
             console.error("Error: carritoId no encontrado");
         }
@@ -72,12 +75,12 @@ const VerCarrito = ({ onContinue }) => {
                         />
                     ))
                 ) : (
-                    <p className="text-white">El carrito está vacío.</p>  
+                    <p className="text-white">El carrito está vacío.</p>
                 )}
             </div>
             <div className="bg-neutral w-[500px] h-[300px] p-6 rounded-lg shadow-lg">
                 <h1 className="text-3xl font-semibold text-white mb-4 text-center">Confirmar Compra</h1>
-    
+
                 <div className="flex flex-col space-y-3 text-white">
                     <div className="flex justify-between">
                         <h2 className="text-xl font-medium">Total Estimado:</h2>
@@ -85,14 +88,14 @@ const VerCarrito = ({ onContinue }) => {
                     </div>
                     <div className="flex justify-between">
                         <h2 className="text-xl font-medium">Cantidad de Productos:</h2>
-                        <p className="text-xl">{cartItems.length}</p>
+                        <p className="text-xl">{totalProductos}</p> 
                     </div>
                 </div>
-    
+
                 <div className="mt-8 flex justify-center">
                     <button
                         className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-3 px-6 w-full rounded-md shadow-lg transition duration-200"
-                        onClick={onContinue} 
+                        onClick={onContinue}
                     >
                         Continuar al Pago
                     </button>
