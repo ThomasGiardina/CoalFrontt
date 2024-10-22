bad11f1881213017f131546ff0d976a8554b3940
ThomasGiardina <thomasagustingiardina@gmail.com>
2024-10-21 23:29:42 -0300

    telefonoRegister

--- src/components/Register/InputsRegister.jsx
+++ src/components/Register/InputsRegister.jsx
@@ -76,28 +76,6 @@ const InputsRegister = ({ formData, setFormData }) => {
                 />
             </label>
 
-            {/* Agregar el campo Teléfono */}
-            <label className="input input-bordered flex items-center gap-2 w-[520px]">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70"
-                >
-                    <path
-                        d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
-                    />
-                </svg>
-                <input
-                    type="text"
-                    name="telefono"
-                    value={formData.telefono}
-                    onChange={handleChange}
-                    className="grow"
-                    placeholder="Teléfono"
-                />
-            </label>
-
             </div>
 
             <label className="input input-bordered flex items-center gap-2 w-[520px]">
