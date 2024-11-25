08f5b2ba052ffd6d787a507a9847342a6b57b200
MarcoAmbrosini22 <mambrosini@uade.edu.ar>
2024-11-25 19:39:21 -0300

    EsteticaOlvidarPass

--- src/components/Login/ForgotPassword.jsx
+++ src/components/Login/ForgotPassword.jsx
@@ -64,7 +64,7 @@ const ForgotPassword = () => {
 
     return (
         <div>
-            <a href="#" onClick={() => setModalIsOpen(true)} className="text-blue-500 hover:underline">
+            <a href="#" onClick={() => setModalIsOpen(true)} className="text-blue-500 hover:underline text-lg">
                 ¿Olvidaste tu Contraseña?
             </a>
             <Modal
@@ -74,12 +74,12 @@ const ForgotPassword = () => {
                 className="flex justify-center items-center h-screen z-50"
                 overlayClassName="fixed inset-0 bg-black bg-opacity-50 z-40 flex justify-center items-center"
             >
-                <div className="relative h-[550px] w-[650px] rounded-xl flex flex-col bg-neutral-950 p-6">
-                    <button onClick={() => setModalIsOpen(false)} className="absolute top-2 right-2 text-gray-300 hover:text-white">
+                <div className="relative w-full max-w-4xl rounded-xl flex flex-col bg-neutral-950 p-10">
+                    <button onClick={() => setModalIsOpen(false)} className="absolute top-2 right-2 text-red-500 hover:text-red-700 text-3xl">
                         &times;
                     </button>
-                    <h1 className="font-bold text-3xl text-orange-500 mb-4 text-center">Restablecer Contraseña</h1>
-                    <p className="mb-6 text-center text-gray-300">Aquí puedes restablecer tu contraseña</p>
+                    <h1 className="font-bold text-5xl text-orange-500 mb-8 text-center">Restablecer Contraseña</h1>
+                    <p className="mb-10 text-center text-gray-300 text-xl">Aquí puedes restablecer tu contraseña</p>
                     <InputsNewPassword onPasswordChange={handlePasswordChange} resetInputs={resetInputs} />
                 </div>
             </Modal>
