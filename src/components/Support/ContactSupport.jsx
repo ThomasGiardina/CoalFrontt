ce95ad4dbff2417be7b6e374689191af23e6191a
ThomasGiardina <thomasagustingiardina@gmail.com>
2024-11-26 00:07:16 -0300

    Color Letra

--- src/components/Support/ContactSupport.jsx
+++ src/components/Support/ContactSupport.jsx
@@ -69,7 +69,7 @@ const ContactSupport = () => {
                 Formulario de Contacto
             </motion.h1>
             <motion.p
-                className="text-lg text-white mb-6 relative z-10"
+                className="text-lg text-white    mb-6 relative z-10"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ delay: 0.2, duration: 1 }}
